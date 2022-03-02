832cc97adb7ce03bbcb713c367fc70e2c92c29c2
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-03-01 22:25:02 -0600

    use file reader

--- PyPoll.py
+++ PyPoll.py
@@ -12,12 +12,23 @@ file_to_save=os.path.join("analysis","election_analysis.txt")
 # 1.2 Open election results and read file
 with open(file_to_load) as election_data:
 
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+        
 # 1.3.1 Open the file
    
     # Write three counties to the file.
     
 # 2. Get total number of votes casted
+
 # 3. Get list of candidates
+
 # 4. Get total votes per candidate
+
 # 5. Calculate vote percentages per candidates
+
 # 6. Identify candidate with most votes, aka winner
