f32cf72c93958d32b99697899829dc993a201010
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-03-01 22:18:51 -0600

    PyPoll getting data

--- PyPoll.py
+++ PyPoll.py
@@ -5,26 +5,19 @@ import os
 
 file_to_load=os.path.join("Resources","election_results.csv")
 
+#1.2 Create a file for results
+
+file_to_save=os.path.join("analysis","election_analysis.txt")
+
 # 1.2 Open election results and read file
 with open(file_to_load) as election_data:
 
-    #print file objecy
-    print(election_data)
-
-#1.3 Create a file for results
-file_to_save=os.path.join("analysis","election_analysis.txt")
-
 # 1.3.1 Open the file
-with open(file_to_save,"w") as txt_file:
-
    
-     # Write three counties to the file.
+    # Write three counties to the file.
     
 # 2. Get total number of votes casted
 # 3. Get list of candidates
 # 4. Get total votes per candidate
 # 5. Calculate vote percentages per candidates
 # 6. Identify candidate with most votes, aka winner
-
-# 7. Close file
-
